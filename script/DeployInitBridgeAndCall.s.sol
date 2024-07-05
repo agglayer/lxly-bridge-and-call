665af141259bb475fd1b1ad449bf30db7353a531
omnifient <90161786+omnifient@users.noreply.github.com>
2024-07-05 17:29:21 +0100

    modify deploy script to use PolygonZkEVMDeployer instead of Forge's Create2 factory
    NOTE: this means the deployment must be executed by the owner of the deployer

--- script/DeployInitBridgeAndCall.s.sol
+++ script/DeployInitBridgeAndCall.s.sol
@@ -2,23 +2,36 @@ pragma solidity 0.8.20;
 
 import "forge-std/Script.sol";
 
+import "@zkevm/deployment/PolygonZkEVMDeployer.sol";
+
 import "src/BridgeExtension.sol";
 import "src/BridgeExtensionProxy.sol";
 
 contract DeployInitBridgeAndCall is Script {
     function run() external {
         // get the required env values
+        address deployerAddr = vm.envAddress("ADDRESS_POLYGON_ZKEVM_DEPLOYER");
         address proxyAdmin = vm.envAddress("ADDRESS_PROXY_ADMIN");
         address bridgeAddr = vm.envAddress("ADDRESS_LXLY_BRIDGE");
-        bytes32 salt = bytes32(uint256(1));
 
+        // setup
+        bytes32 salt = bytes32(uint256(1));
+        PolygonZkEVMDeployer deployer = PolygonZkEVMDeployer(deployerAddr);
+
+        // first deploy the implementation, then the proxy+initialize it
+        // this is done using create2 under the hood
         vm.startBroadcast(vm.envUint("DEPLOYER_PRIVATE_KEY"));
 
-        // deploy the implementation contract
-        BridgeExtension beImpl = new BridgeExtension{salt: salt}();
+        // get the deterministic address for the implementation contract
+        bytes memory beImplBytecode = type(BridgeExtension).creationCode;
+        address beImplAddr = deployer.predictDeterministicAddress(salt, keccak256(beImplBytecode));
+        // deploy the implementation contract using the PolygonZkEVMDeployer
+        deployer.deployDeterministic(0, salt, beImplBytecode);
 
-        // deploy the proxy and initialize (runs behind the scenes through the proxy)
-        new BridgeExtensionProxy{salt: salt}(proxyAdmin, address(beImpl), bridgeAddr);
+        // deploy the proxy and initialize (runs behind the scenes in the proxy's constructor)
+        bytes memory beProxyBytecode =
+            abi.encodePacked(type(BridgeExtensionProxy).creationCode, abi.encode(proxyAdmin, beImplAddr, bridgeAddr));
+        deployer.deployDeterministic(0, salt, beProxyBytecode);
 
         vm.stopBroadcast();
     }
